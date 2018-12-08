202833d3187bc6498accea555450dd514fb46b4c
karantine <12798243+karantine@users.noreply.github.com>
2018-12-08 23:54:56 +0300

    solution added

---
+++ context_manager.py
@@ -0,0 +1,27 @@
+import time
+
+
+class ContextManager:
+    def __enter__(self):
+        start_time = time.time()
+        print('Начало работы программы: {}'.format(time.ctime(start_time)))
+        return start_time
+
+    def __exit__(self, type, value, traceback):
+        end_time = time.time()
+        print('Окончание работы программы: {}'.format(time.ctime(end_time)))
+        sum_time = end_time - start_time
+        print('Время работы программы: {} с.'.format(sum_time))
+
+
+def calculate_num():
+    x = float(input('Введите число:'))
+    y = float(input('Введите число:'))
+    total_num = (x*y + 18)/2
+    print('Результат вычисления: {}'.format(total_num))
+    return total_num
+
+
+if __name__ == "__main__":
+    with ContextManager() as start_time:
+        calculate_num()
